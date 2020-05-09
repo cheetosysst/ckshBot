ec9056243bc0e47328725ab6e57b0776de2448d3
cheetosysst <x9930707@gmail.com>
2020-05-09 09:42:20 +0800

    Some basic stuff

---
+++ main.py
@@ -0,0 +1,6 @@
+# School bot
+# Run with Python3, require re and requests.
+
+import school as sc
+
+print("[] School Bot test")
---
+++ school/school.py
@@ -0,0 +1,19 @@
+import re
+import requests
+
+class school():
+    def school(self):
+
+        self.project    = "ckshBot"
+        self.version    = "0.0.1"
+        self.devState   = "alpha"
+        self.officeCode = 0
+
+    def setOffice(self, officeCode):
+        self.officeCode = officeCode
+
+    def getData(self):
+        # TODO
+
+        return data
+
